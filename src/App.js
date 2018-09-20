e34e1b3d21e0d1487b2764f39a67b24e91f543f9
Tien Borland <nguy2819@crk.umn.edu>
2018-09-19 20:41:55 -0600

    first and final commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>It will speak "My wonderful husband, I always love you" - under a German Female voice</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import allVoices from './voice';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const msg = new SpeechSynthesisUtterance();
+msg.volume = 1; // 0 to 1
+msg.rate = 1; // 0.1 to 10
+msg.pitch = 1; //0 to 2
+msg.text = "My wonderful husband, I always love you";
+
+const voice = allVoices[4] // 47 total
+console.log(`voice: ${voice.name}, language: ${voice.lang}`)
+msg.voiceURI = voice.voiceURI;
+msg.lang = voice.lang;
+
+speechSynthesis.speak(msg);
